77789cc9b5eaf0983d9e5a58deaf1dc968bc1279
wuyeelok <wuyeelok@gmail.com>
2025-02-19 10:00:19 +0800

    more example

--- Program.cs
+++ Program.cs
@@ -9,5 +9,21 @@ foreach (string name in names)
     Console.WriteLine($"Hello {name.ToUpper()}!");
 }
 
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine($"Hello {names[i].ToUpper()}!");
+}
+
+
 var items = new List<string>();
 Console.WriteLine($"There are {items.Count} items in the list.");
+
+var keys = new HashSet<string>
+{
+    "key1",
+    "key1"
+};
+foreach (var key in keys)
+{
+    Console.WriteLine(key);
+}
